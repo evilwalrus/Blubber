8c731e16a7d5eda233bba4bb77054bfd2b8b33cf
Andrew Heebner <andrew.heebner@gmail.com>
2015-03-26 11:41:25 -0400

    Huge bug in Response
    Bad bug causing hangs when checking for gzip.

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ class TestApi extends \Blubber\App
 {
 
     private $options = array(
-        'use_output_compression'  => true,
+        'use_output_compression'  => false, // set to 'true' to use builtin gzip; otherwise, use apache's gzip
         'require_user_agent'      => false,
         'redirect_old_namespaces' => true,
     );
